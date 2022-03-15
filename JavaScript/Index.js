fa6a7a2bb1b830014196f72efe0fa61896188612
{null} <83356797+BLOBBOBE@users.noreply.github.com>
2022-03-15 13:36:25 -0400

    Create Index.js

---
+++ JavaScript/Index.js
@@ -0,0 +1,13 @@
+var CurrentDate = new Date();
+
+    var datetime = "Last Sync: " + CurrentDate.getDate() + "/"+(CurrentDate.getMonth()+1) 
+    + "/" + CurrentDate.getFullYear() + " @ " 
+    + CurrentDate.getHours() + ":" 
+    + CurrentDate.getMinutes() + ":" + CurrentDate.getSeconds();
+
+
+//console.log(CurrentDate)
+
+var Info = CurrentDate 
+
+console.log(Info)
