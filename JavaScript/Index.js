303a65404033674d805d3db08195f73450b997bf
Landon <91854954+killrebeest@users.noreply.github.com>
2022-03-23 21:15:59 -0400

    Added A Crasher and a IP Logger

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,13 +1,26 @@
 var CurrentDate = new Date();
 
-    var datetime = "Last Sync: " + CurrentDate.getDate() + "/"+(CurrentDate.getMonth()+1) 
-    + "/" + CurrentDate.getFullYear() + " @ " 
-    + CurrentDate.getHours() + ":" 
-    + CurrentDate.getMinutes() + ":" + CurrentDate.getSeconds();
-
+var datetime =
+	"Last Sync: " +
+	CurrentDate.getDate() +
+	"/" +
+	(CurrentDate.getMonth() + 1) +
+	"/" +
+	CurrentDate.getFullYear() +
+	" @ " +
+	CurrentDate.getHours() +
+	":" +
+	CurrentDate.getMinutes() +
+	":" +
+	CurrentDate.getSeconds();
 
 //console.log(CurrentDate)
 
-var Info = CurrentDate 
+var Info = CurrentDate;
 
-console.log(Info)
+console.log(Info);
+
+function editText(id, text) {
+	var element = document.getElementById(id);
+	element.innerHTML = text;
+}
